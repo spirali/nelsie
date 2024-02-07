50dfbb18b3e224e7636f80655a0864eed6cc210d
Ada Bohm <ada@kreatrix.org>
2024-02-07 15:31:15 +0100

    Better CI

--- scripts/format.sh
+++ scripts/format.sh
@@ -6,25 +6,5 @@ DIRS="tests python"
 
 set -e
 
-if [[ "$1" != "--check" && "x$1" != "x" ]]; then
-    echo "Run as '$0 --check' or just '$0'"
-    exit 1
-fi
-
-cd `dirname $0`/..
-
-if [[ "$1" == "--check" ]]; then
-    python -m isort --check-only --profile black $DIRS
-    python -m black --check $DIRS
-    # Lint Python code
-    # By default, this is skipped in checking
-    #poetry run flake8 tests interlab
-    echo "All code checks completed successfully"
-else
-    # Format Python code
-    python -m isort --profile black $DIRS
-    python -m black $DIRS
-    # Lint Python code
-    python -m flake8 $DIRS
-fi
-
+python -m ruff format --check $DIRS
+python -m ruff check $DIRS
