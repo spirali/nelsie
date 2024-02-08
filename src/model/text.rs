084058f1eb43bb355bfa0ddee2d36d7ebf048c09
Ada Bohm <ada@kreatrix.org>
2024-02-08 17:15:13 +0100

    Enabled clippy in CI

--- src/model/text.rs
+++ src/model/text.rs
@@ -32,16 +32,6 @@ pub(crate) struct StyledLine {
 }
 
 impl StyledLine {
-    pub fn line_height(&self, text_styles: &[TextStyle]) -> Option<f32> {
-        self.spans
-            .iter()
-            .map(|span| {
-                let style = &text_styles[span.style_idx as usize];
-                style.size * style.line_spacing
-            })
-            .max_by(|x, y| x.partial_cmp(y).unwrap())
-    }
-
     pub fn line_descender(&self, text_styles: &[TextStyle]) -> Option<f32> {
         self.spans
             .iter()
@@ -90,24 +80,6 @@ impl StyledText {
                 }
             })
             .sum()
-    }
-
-    pub fn font_size(&self, line_idx: usize) -> f32 {
-        self.styled_lines
-            .get(line_idx)
-            .and_then(|line| line.font_size(&self.styles))
-            .unwrap_or(self.default_font_size)
-    }
-
-    pub fn default_line_height(&self) -> f32 {
-        self.default_line_spacing * self.default_font_size
-    }
-
-    pub fn line_height(&self, line_idx: usize) -> f32 {
-        self.styled_lines
-            .get(line_idx)
-            .and_then(|line| line.line_height(&self.styles))
-            .unwrap_or_else(|| self.default_line_height())
     }
 }
 
