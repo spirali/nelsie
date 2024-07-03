e0c6013ae702c6566045cd2a18735af719549f4c
Galen Seilis <galen.seilis@seilis.ca>
2024-07-03 15:15:29 -0700

    Fix spelling typo bigdemo.py

--- examples/bigdemo/bigdemo.py
+++ examples/bigdemo/bigdemo.py
@@ -493,7 +493,7 @@ def chess_board(slide):
 
 @deck.slide()
 def links(slide):
-    slide.text("Clicable links in PDF", TextStyle(size=80, underline=True), url="https://github.com/spirali/nelsie")
+    slide.text("Clickable links in PDF", TextStyle(size=80, underline=True), url="https://github.com/spirali/nelsie")
 
 
 # Debugging frames ##########################################
