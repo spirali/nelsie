a4587dc5725917d0242ae18f7ad7984a6c225913
Ada Bohm <ada@kreatrix.org>
2024-10-29 22:17:15 +0100

    Added support for direct ellipse in PDF

--- src/common/path.rs
+++ src/common/path.rs
@@ -82,6 +82,16 @@ impl PathBuilder {
         })
     }
 
+    pub fn last_point(&self) -> (f32, f32) {
+        match self.0.parts.last() {
+            Some(PathPart::Move { x, y })
+            | Some(PathPart::Line { x, y })
+            | Some(PathPart::Quad { x, y, .. })
+            | Some(PathPart::Cubic { x, y, .. }) => (*x, *y),
+            None | Some(PathPart::Close) => (0.0, 0.0),
+        }
+    }
+
     pub fn move_to(&mut self, x: f32, y: f32) {
         self.0.parts.push(PathPart::Move { x, y })
     }
@@ -100,6 +110,46 @@ impl PathBuilder {
             x,
             y,
         })
+    }
+
+    pub fn arc_to(
+        &mut self,
+        rx: f32,
+        ry: f32,
+        x_axis_rotation: f32,
+        large_arc: bool,
+        sweep: bool,
+        x: f32,
+        y: f32,
+    ) {
+        let (last_x, last_y) = self.last_point();
+
+        let svg_arc = kurbo::SvgArc {
+            from: kurbo::Point::new(last_x as f64, last_y as f64),
+            to: kurbo::Point::new(x as f64, y as f64),
+            radii: kurbo::Vec2::new(rx as f64, ry as f64),
+            x_rotation: (x_axis_rotation as f64).to_radians(),
+            large_arc,
+            sweep,
+        };
+
+        match kurbo::Arc::from_svg_arc(&svg_arc) {
+            Some(arc) => {
+                arc.to_cubic_beziers(0.1, |p1, p2, p| {
+                    self.cubic_to(
+                        p1.x as f32,
+                        p1.y as f32,
+                        p2.x as f32,
+                        p2.y as f32,
+                        p.x as f32,
+                        p.y as f32,
+                    );
+                });
+            }
+            None => {
+                self.line_to(x, y);
+            }
+        }
     }
 
     pub fn close(&mut self) {
